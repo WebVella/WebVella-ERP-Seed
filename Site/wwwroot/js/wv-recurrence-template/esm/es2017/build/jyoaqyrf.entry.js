6ae3e09d3fd32d64f8198fb9e146e34a77007c84
Rumen Yankov <rumen.yankov@gmail.com>
2019-02-26 12:46:32 +0200

    updates nuget packages amnd wwwroot resources to latest version

--- Site/wwwroot/js/wv-recurrence-template/esm/es2017/build/jyoaqyrf.entry.js
+++ Site/wwwroot/js/wv-recurrence-template/esm/es2017/build/jyoaqyrf.entry.js
@@ -1,5 +1,28 @@
-import RecurrenceTemplate from '../../models/RecurrenceTemplate';
-export class WvDatasourceManage {
+import { h } from '../wv-recurrence-template.core.js';
+
+class RecurrenceTemplate {
+    constructor() {
+        this.type = 0;
+        this.end_type = 0;
+        this.end_date = null;
+        this.end_count = 1;
+        this.repeat_period_type = 0;
+        this.interval = 1;
+        this.timespan_start = null;
+        this.timespan_end = null;
+        this.allow_monday = true;
+        this.allow_tuesday = true;
+        this.allow_wednesday = true;
+        this.allow_thursday = true;
+        this.allow_friday = true;
+        this.allow_saturday = true;
+        this.allow_sunday = true;
+        this.repeat_month_type = 0;
+        this.recurrence_change_type = 0;
+    }
+}
+
+class WvDatasourceManage {
     constructor() {
         this.recurrenceTemplate = null;
         this.templateDefault = null;
@@ -169,3 +192,5 @@ export class WvDatasourceManage {
         }
     }; }
 }
+
+export { WvDatasourceManage as WvRecurrenceTemplate };
